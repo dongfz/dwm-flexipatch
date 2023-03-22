15aad3513f4216e5bff2cd983836b9cb764de45a
dfz <sneakydog@yeah.net>
2023-03-22 23:15:46 +0800

    Modify autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -4,6 +4,11 @@ export QT_IM_MODULE=fcitx
 export XMODIFIERS=@im=fcitx
 export SDL_IM_MODULE=fcitx
 export GLFW_IM_MODULE=ibus
+export https_proxy=http://localhost:7890
+export http_proxy=http://localhost:7890
+
+
+/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
 feh --recursive --bg-fill --randomize ~/dwm-flexipatch/wallpaper &
 picom &
