902d9e94672d5585fded423e37297848ff9761de
dfz <sneakydog@yeah.net>
2023-03-22 22:31:06 +0800

    Modify autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,3 +1,10 @@
+
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
+export SDL_IM_MODULE=fcitx
+export GLFW_IM_MODULE=ibus
+
 feh --recursive --bg-fill --randomize ~/dwm-flexipatch/wallpaper &
 picom &
 fcitx5 &
